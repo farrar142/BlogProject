5eff65fcd79328651495e4f11d243f7d167f0a01
farrar142 <gksdjf1690@gmail.com>
2022-08-10 01:30:27 +0900

    fix:잘못된 객체분해 수정

--- frontend/pages/accounts/signout.tsx
+++ frontend/pages/accounts/signout.tsx
@@ -1,30 +1,30 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useSysMsg } from '../../components/MySnackBar';
-import { useUserInfo } from '../../src/atoms';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSysMsg } from "../../components/MySnackBar";
+import { useUserInfo } from "../../src/atoms";
 import {
   deleteCookie,
   getCookie,
   setCookie,
-} from '../../src/functions/cookies';
-import getUserInfo, { UserInfoDefault } from '../../src/hooks/getUserInfo';
-import { UserInfo } from '../../types/accounts';
+} from "../../src/functions/cookies";
+import getUserInfo, { UserInfoDefault } from "../../src/hooks/getUserInfo";
+import { UserInfo } from "../../types/accounts";
 
 const Signout = () => {
   const router = useRouter();
   const [msg, setMsg] = useSysMsg();
-  const [userInfo, setUserInfo, resetter] = useUserInfo();
+  const [userInfo, setUserInfo] = useUserInfo();
   useEffect(() => {
-    const token = getCookie('token');
+    const token = getCookie("token");
     if (token) {
-      deleteCookie('token');
+      deleteCookie("token");
       setUserInfo(UserInfoDefault);
       router.back();
-      setMsg({ type: 'success', message: '로그아웃되었습니다' });
+      setMsg({ type: "success", message: "로그아웃되었습니다" });
     } else {
       setUserInfo(UserInfoDefault);
-      setMsg({ type: 'warning', message: '잘못된 접근' });
-      router.push('/');
+      setMsg({ type: "warning", message: "잘못된 접근" });
+      router.push("/");
     }
   }, []);
   return <div></div>;
