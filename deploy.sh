b0fcf58630bf706ce2fe4962329c5a5d184de1a8
farrar142 <farrar142@github.com>
2022-08-09 17:45:45 +0900

    fix:젠킨스빌드 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.deploy.yml build --force-rm
-docker-compose -f docker-compose.deploy.yml up -d
+docker-compose -f docker-compose.deploy.yaml build --force-rm
+docker-compose -f docker-compose.deploy.yaml up -d
 #
